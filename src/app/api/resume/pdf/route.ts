779150b7cfd507bc65fe33c00c5a5a5876a51c24
Jacob9610 <vazquezjake@live.com>
2025-08-30 11:50:14 -0700

    bug fix for build on vercel

--- src/app/api/resume/pdf/route.ts
+++ src/app/api/resume/pdf/route.ts
@@ -6,7 +6,7 @@ import ResumePDF from "@/components/ResumePDF";
 export async function GET() {
   const stream = await renderToStream(React.createElement(ResumePDF));
 
-  return new NextResponse(stream as any, {
+  return new NextResponse(stream as unknown as ReadableStream<Uint8Array>, {
     headers: {
       "Content-Type": "application/pdf",
       "Content-Disposition": 'attachment; filename="Jacob-Vazquez-Resume.pdf"',
